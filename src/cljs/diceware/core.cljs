a5467acc2ddae37f07802c4a962468cde8b98809
Lawrence Allan Jones <mrwizard82d1@gmail.com>
2016-01-03 09:26:02 -0600

    Partial implementation of view.
    Write code to view the practice text area and the Go! button. Write code
    to dynamically generate the candidate passwords. Write code to handle
    changing the number of generated passwords.
    
    To my very pleasant surprise, when I change the number of generated
    passwords, the dynamically generated (empty) list of candidate
    passwords. Although it is great to see that dynamism, I really do not
    want it to change until I press "Go!" I'll need to make this change.

--- src/cljs/diceware/core.cljs
+++ src/cljs/diceware/core.cljs
@@ -2,9 +2,61 @@
   (:require [reagent.core :as reagent]
             [cljsjs.react]))
 
+(def app-state
+  (reagent/atom
+    {:options {:count 5}
+     :results {:candidates []}}))
+
+(enable-console-print!)
+
+(defn get-how-many []
+  (get-in @app-state [:options :count]))
+
+(defn view-generators [])
+
+(defn view-how-many []
+  [:div {:class-name "how-many vertical-isolation"}
+    [:label {:for "count"} "How Many?"]
+    [:input {:type      "number" :name "count" :id "count" :value (get-how-many) :min 1 :max 17
+             :on-change (fn [e] (swap! app-state update-in [:options :count] (fn [_] (-> e .-target .-value)))
+                          #_(do
+                              (println e)
+                              (println (-> e .-target))
+                              (println (-> e .-target .-value))))}]])
+
+(defn options []
+  [:section {:id "options" :class-name "col-4"}
+   [:h2 "Options"]
+   [:form
+    (view-generators)
+    (view-how-many)
+    [:input {:type "submit" :class-name "span-width vertical-isolation" :value "Go!" :id "go"}]]])
+
+(defn view-candidate-passwords []
+  [:ul
+   (let [candidate-passwords (get-in @app-state [:results :candidates])]
+     (if (not (empty? candidate-passwords))
+       (map #([:li %]) candidate-passwords)
+       (map-indexed (fn [i _] ^{:key i} [:li]) (range (get-how-many)))))])
+
+(defn results []
+  [:section {:id "results" :class-name "col-8"}
+   [:section {:class-name "generated"}
+    [:h2 "Generated"]
+    (view-candidate-passwords)]
+   [:section {:class-name "practice"}
+    [:h2 "Practice"]
+    [:textarea {:rows (get-how-many)
+                :cols 55}]]])
+
 (defn main-page
   []
-  [:div "Hello World!"])
+  [:header
+   [:h1 "Password Generator"]
+   [:section {:class-name "row"}
+    (options)
+    (results)
+    ]])
 
 (defn mount-root
   []

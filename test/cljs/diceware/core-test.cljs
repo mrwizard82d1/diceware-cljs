867fe3e18a9daaa56ae60634cf5a0125089b4e22
Lawrence Allan Jones <mrwizard82d1@gmail.com>
2016-01-10 15:04:21 -0600

    Add tests for candidate passwords.
    Add two tests for viewing candidate passwords:
    
    - No passwords requested.
    - One password requested.

--- test/cljs/diceware/core-test.cljs
+++ test/cljs/diceware/core-test.cljs
@@ -5,7 +5,14 @@
 (enable-console-print!)
 
 (t/deftest view-candidate-passwords-test
-           (t/is (= [:ul (repeat 5 [:li])] (dc/view-candidate-passwords)))
+           (t/testing "Initial application state"
+                    (t/is (= [:ul (repeat 5 [:li])] (dc/view-candidate-passwords))))
+           (t/testing "No passwords requested"
+                      (do (swap! dc/app-state update-in [:options :as-is :count] (fn [_old] 0))
+                          (t/is (= [:ul (range 0)] (dc/view-candidate-passwords)))))
+           (t/testing "One password requested"
+                      (do (swap! dc/app-state update-in [:options :as-is :count] (fn [_old] 1))
+                          (t/is (= [:ul (seq [[:li]])] (dc/view-candidate-passwords)))))
            )
 
 (set! *main-cli-fn* #(t/run-tests))

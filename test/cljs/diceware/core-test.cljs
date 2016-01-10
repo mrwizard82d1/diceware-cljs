741827e08a0fb11d81b019360134822b3b7bd03f
Lawrence Allan Jones <mrwizard82d1@gmail.com>
2016-01-10 00:07:18 -0600

    Change test to passing.

--- test/cljs/diceware/core-test.cljs
+++ test/cljs/diceware/core-test.cljs
@@ -1,9 +1,11 @@
 (ns diceware.core-test
-  (:require [cljs.test :as t]))
+  (:require [cljs.test :as t]
+            [diceware.core :as dc]))
 
 (enable-console-print!)
 
-(t/deftest my-first-test
-  (t/is (= 1 2)))
+(t/deftest view-candidate-passwords-test
+           (t/is (= [:ul (repeat 5 [:li])] (dc/view-candidate-passwords)))
+           )
 
 (set! *main-cli-fn* #(t/run-tests))

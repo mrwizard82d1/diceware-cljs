afca3b038cf4d5cebef6b542af6797850c5dcb61
Lawrence Allan Jones <mrwizard82d1@gmail.com>
2016-01-14 02:54:16 -0600

    Verify generating multiple passphrases.

--- test/cljs/diceware/generators-test.cljs
+++ test/cljs/diceware/generators-test.cljs
@@ -3,17 +3,21 @@
             [clojure.string :as str]
             [diceware.generators :as dg]))
 
+(def five-words ["vetus" "mensae" "filia" "Europae" "bracteae"])
+
+(def sequential-selector
+  (let [counter (atom 0)]
+    (fn [words]
+      (let [word (nth words (mod @counter (count words)))]
+        (swap! counter inc)
+        word))))
+
 (t/deftest generate-passphrase
            (t/is (= 0 (count (dg/generate :passphrase 0 ["a"] (fn [ws] (nth ws 0))))))
-           (let [single-passphrases (dg/generate :passphrase 1
-                                                ["vetus" "mensae" "filia" "Europae" "bracteae"]
-                                                (let [counter (atom 0)]
-                                                  (fn [words]
-                                                    (let [n @counter
-                                                          word (nth words (mod n (count words)))]
-                                                      (swap! counter inc)
-                                                      word))))]
+           (let [single-passphrases (dg/generate :passphrase 1 five-words sequential-selector)]
              (t/is (= 1 (count single-passphrases)))
              (let [pieces (str/split (nth single-passphrases 0) " ")]
                (t/is (= 5 (count pieces)))
-               (t/is (= ["vetus" "mensae" "filia" "Europae" "bracteae"] pieces)))))
+               (t/is (= five-words pieces)))
+             (t/is (= 3 (count (dg/generate :passphrase 3 five-words sequential-selector))))))
+
